154cce7b5bdf0b6628044301f84f1d1f3426317d
xrio <elattar.saad.3@gmail.com>
2021-09-10 19:27:59 +0100

    xrio : adding movie model

---
+++ src/main/java/io/xrio/movies/model/Movie.java
@@ -0,0 +1,28 @@
+package io.xrio.movies.model;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+/**
+ * @author : Elattar Saad
+ * @version 1.0
+ * @since 10/9/2021
+ */
+@Data
+@Entity
+public class Movie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+    private String name;
+    private String type;
+    private Long duration;
+    private Date releaseDate;
+
+}
